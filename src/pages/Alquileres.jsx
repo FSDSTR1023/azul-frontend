ebc8643139fc08ff1937d50382074a765d22d506
Raisndev <mseresagusil@gmail.com>
2024-01-07 12:45:15 +0100

    added routes of app

---
+++ src/pages/Alquileres.jsx
@@ -0,0 +1,5 @@
+export const Alquileres = () => {
+  return (
+    <div>Alquileres</div>
+  )
+}
---
+++ src/pages/Incidencias.jsx
@@ -0,0 +1,6 @@
+export const Incidencias = () => {
+  return (
+    <div>Incidencias</div>
+  )
+}
+
