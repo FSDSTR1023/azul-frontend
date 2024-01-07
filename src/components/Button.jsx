3f9cefc67299a7792b9f49f1edd0b865f4451621
Raisndev <mseresagusil@gmail.com>
2024-01-07 21:42:12 +0100

    created components: input buttons

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+export const Button = ({ className, type, text }) => {
+    const addClass = className ? className : ''
+    return (
+        <button
+            className={addClass + ' py-2 px-4 text-white bg-slate-800 hover:bg-slate-600 rounded transition '}
+            type={type}
+        >
+            {text}
+        </button>
+    )
+}
