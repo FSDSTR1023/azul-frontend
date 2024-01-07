27dd4916c46180d20bd27a6e8981ebbf86b404c6
Raisndev <mseresagusil@gmail.com>
2024-01-07 21:43:09 +0100

    correcting some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
 import { Home } from './pages/Home'
 import { Dashboard } from './pages/Dashboard'
 import { Alquileres } from './pages/Alquileres'
 import { Incidencias } from './pages/Incidencias'
 import { Users } from './pages/Users'
 import { LoginPage } from './pages/LoginPage'
+import { AuthProvider } from './context/AuthContext'
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/dasboard" element={<Dashboard />} />
-                <Route path="/usuarios" element={<Users />} />
-                <Route path="/alquileres" element={<Alquileres />} />
-                <Route path="/incidencias" element={<Incidencias />} />
-            </Routes>
-        </BrowserRouter>
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/dasboard" element={<Dashboard />} />
+                    <Route path="/usuarios" element={<Users />} />
+                    <Route path="/alquileres" element={<Alquileres />} />
+                    <Route path="/incidencias" element={<Incidencias />} />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
     )
 }
 
