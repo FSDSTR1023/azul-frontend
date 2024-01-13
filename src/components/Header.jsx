02780449f589f948df5a9d514cbe7264395724b4
Raisn.io <mseresagusil@gmail.com>
2024-01-13 18:10:36 +0100

    header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { Button } from './Button'
+import { AddIcon } from './Icons'
+
+export const Header = ({ pageName, buttonText, className, visibleBtn, visibleSearch }) => {
+    return (
+        <header className={`flex gap-4 ${className} p-5`}>
+            <h1 className="flex-1">{pageName}</h1>
+            {visibleSearch && (
+                <form action="">
+                    <input className="px-4 py-2 rounded-md" type="text" placeholder="Buscar..." />
+                </form>
+            )}
+
+            {visibleBtn && (
+                <Button>
+                    <AddIcon />
+                    {buttonText}
+                </Button>
+            )}
+        </header>
+    )
+}
