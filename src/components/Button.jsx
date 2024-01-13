cbddfa433699fe6262bd84673ae5c17f8cb0ee08
Raisn.io <mseresagusil@gmail.com>
2024-01-13 10:17:12 +0100

    adding onClick functionality to component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,10 @@
-export const Button = ({ className, type, text }) => {
+export const Button = ({ className, type, text, onClick }) => {
     const addClass = className ? className : ''
     return (
         <button
             className={addClass + ' py-2 px-4 text-white bg-slate-800 hover:bg-slate-600 rounded transition '}
             type={type ? type : 'button'}
+            onClick={onClick}
         >
             {text}
         </button>
