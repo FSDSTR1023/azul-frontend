5e2c8ab4ada098bcb47b4b94a31c78727c959312
Raisn.io <mseresagusil@gmail.com>
2024-01-13 18:09:04 +0100

    button modif for getting all childrens

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
-export const Button = ({ className, type, text, onClick }) => {
-    const addClass = className ? className : ''
+export const Button = ({ className, type, onClick, children }) => {
+    const addedClass = className ? className : ''
     return (
         <button
-            className={addClass + ' py-2 px-4 text-white bg-slate-800 hover:bg-slate-600 rounded transition '}
+            className={`${addedClass} py-2 px-4 text-white bg-slate-800 hover:bg-slate-600 rounded transition flex gap-4 items-center`}
             type={type ? type : 'button'}
             onClick={onClick}
         >
-            {text}
+            {children}
         </button>
     )
 }
