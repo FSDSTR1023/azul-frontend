f74788996855f03311c856a410c19dab8ccfc3a8
Raisn.io <mseresagusil@gmail.com>
2024-01-20 16:40:10 +0100

    migrated to new API

--- src/components/Tabla.jsx
+++ src/components/Tabla.jsx
@@ -1,57 +1,74 @@
-import { useTable, useSortBy } from 'react-table'
+import {
+  useReactTable,
+  getCoreRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  flexRender
+} from '@tanstack/react-table'
+import { SortDownIcon, SortIcon, SortUpIcon } from './Icons'
+export default function Tabla ({ columns, data }) {
+//   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable(
+//     {
+//       columns,
+//       data
+//     },
+//     useSortBy
+//   )
 
-export default function Tabla({ columns, data }) {
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable(
-        {
-            columns,
-            data
-        },
-        useSortBy
-    )
+  const tableRef = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel()
+  })
+  return (
+    <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
+      <table
+        className='w-full text-sm text-left rtl:text-right text-gray-500 '
+      >
+        <thead className='text-xs text-gray-700 uppercase bg-gray-50'>
+          {tableRef.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th className='px-6 py-3' key={header.id} onClick={header.column.getToggleSortingHandler()}>
+                  {header.isPlaceholder
+                    ? null
+                    : (
+                      <div className='flex gap-3 items-center cursor-pointer'>
+                        {
+                            flexRender(header.column.columnDef.header, header.getContext())
+                        }
+                        {
+                            { asc: <SortUpIcon />, desc: <SortDownIcon /> }[header.column.getIsSorted() ?? null] || <SortIcon />
 
-    return (
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table
-                {...getTableProps()}
-                className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
-            >
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    {headerGroups.map((headerGroup) => (
-                        <tr key={headerGroup.id} {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map((column) => (
-                                <th
-                                    className="px-6 py-3"
-                                    key={headerGroup.id}
-                                    {...column.getHeaderProps(column.getSortByToggleProps())}
-                                >
-                                    {column.render('Header')}
-                                    <span>{column.isSorted ? '🔽' : '🔼'}</span>
-                                </th>
-                            ))}
-                        </tr>
-                    ))}
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                    {rows.map((row) => {
-                        prepareRow(row)
-                        return (
-                            <tr
-                                key={row.id}
-                                {...row.getRowProps()}
-                                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-                            >
-                                {row.cells.map((cell) => {
-                                    return (
-                                        <td key={row.id} {...cell.getCellProps()} className="px-6 py-4">
-                                            {cell.render('Cell')}
-                                        </td>
-                                    )
-                                })}
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-        </div>
-    )
+                        }
+                      </div>
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {tableRef.getRowModel().rows.map((row) => {
+            return (
+              <tr
+                key={row.id}
+                className='bg-white border-b'
+              >
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <td key={cell.id} className='px-6 py-4'>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
 }
