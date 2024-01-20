93d481557ac1f794bb2534b0aa85e5b3f5db31f1
Raisn.io <mseresagusil@gmail.com>
2024-01-20 18:06:10 +0100

    solve identing issues

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,26 @@ import { ProtectedRoutes } from './components/ProtectedRoutes'
 import { ALQUILERES, DASBOARD, INCIDENCIAS, LOGIN, MAQUINAS, NOTFOUND, USUARIOS } from './schemas/Navigaton-schema'
 import { NotFound } from './pages/NotFound'
 
-function App() {
-    return (
-        <AuthProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Navigate to={DASBOARD.navigation} />} />
-                    <Route path={LOGIN.navigation} element={<LoginPage />} />
-                    <Route path={NOTFOUND.navigation} element={<NotFound />} />
-                    <Route path="*" element={<Navigate to={NOTFOUND.navigation} />} />
-                    <Route element={<ProtectedRoutes />}>
-                        <Route path={DASBOARD.navigation} element={<Dashboard />} />
-                        <Route path={USUARIOS.navigation} element={<Users />} />
-                        <Route path={ALQUILERES.navigation} element={<Alquileres />} />
-                        <Route path={INCIDENCIAS.navigation} element={<Incidencias />} />
-                        <Route path={MAQUINAS.navigation} element={<Maquinas />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        </AuthProvider>
-    )
+function App () {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navigate to={DASBOARD.navigation} />} />
+          <Route path={LOGIN.navigation} element={<LoginPage />} />
+          <Route path={NOTFOUND.navigation} element={<NotFound />} />
+          <Route path='*' element={<Navigate to={NOTFOUND.navigation} />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path={DASBOARD.navigation} element={<Dashboard />} />
+            <Route path={USUARIOS.navigation} element={<Users />} />
+            <Route path={ALQUILERES.navigation} element={<Alquileres />} />
+            <Route path={INCIDENCIAS.navigation} element={<Incidencias />} />
+            <Route path={MAQUINAS.navigation} element={<Maquinas />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  )
 }
 
 export default App
