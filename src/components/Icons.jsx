9f307d48fbe9ee7005a143a469546de004d16168
Raisn.io <mseresagusil@gmail.com>
2024-01-20 12:13:20 +0100

    added chevron icons

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -158,3 +158,14 @@ export const IssueIcon = ({ className }) => {
     </svg>
   )
 }
+
+export const ChevronDownIcon = ({ className }) => {
+  return (
+    <svg className={className} xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'><path d='m6 9 6 6 6-6' /></svg>
+  )
+}
+export const ChevronUpIcon = ({ className }) => {
+  return (
+    <svg className={className} xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'><path d='m18 15-6-6-6 6' /></svg>
+  )
+}
