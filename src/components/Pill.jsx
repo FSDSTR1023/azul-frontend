933e8115a0eb581596f24bdd3e6fce03a4eeb0e3
Raisn.io <mseresagusil@gmail.com>
2024-01-31 22:52:59 +0100

    added class to avoid line break

--- src/components/Pill.jsx
+++ src/components/Pill.jsx
@@ -1,5 +1,5 @@
 export const Pill = ({ children, color }) => {
   return (
-    <span style={{ backgroundColor: color }} className='py-2 px-3 rounded-full font-semibold text-black '>{children}</span>
+    <span style={{ backgroundColor: color }} className='py-2 px-3 rounded-full font-semibold text-black whitespace-nowrap'>{children}</span>
   )
 }
