6f3b76520f7ab5440627178f04a48c5e1a375bda
Raisn.io <mseresagusil@gmail.com>
2024-01-31 22:51:05 +0100

    added variants to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,15 @@
-export const Button = ({ className, type, onClick, children }) => {
+export const Button = ({ className, type, onClick, children, variant = 'primary' }) => {
   const addedClass = className || ''
+  const variantClasses = {
+    primary: 'bg-slate-800 hover:bg-slate-600 text-white',
+    secondary: 'bg-gray-500 hover:bg-gray-700 text-white',
+    ghost: 'bg-transparent hover:bg-slate-200 text-black',
+    outline: 'bg-transparent hover:bg-slate-200 text-black border border-slate-200',
+    danger: 'bg-red-500 hover:bg-red-700 text-white'
+  }
   return (
     <button
-      className={`${addedClass} py-2 px-4 text-white bg-slate-800 hover:bg-slate-600 rounded transition flex gap-4 items-center`}
+      className={`${addedClass} ${variantClasses[variant]}  py-2 px-4 rounded transition flex gap-1 items-center`}
       type={type || 'button'}
       onClick={onClick}
     >
