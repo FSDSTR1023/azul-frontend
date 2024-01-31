6a37dccacbfcdc7b9ff963f35d459c8cb4e3b694
Raisn.io <mseresagusil@gmail.com>
2024-01-31 23:36:42 +0100

    added custom navlink

--- src/components/CustomLink.jsx
+++ src/components/CustomLink.jsx
@@ -1,9 +1,16 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 export const CustomLink = ({ children, to }) => {
   return (
-    <Link className='py-2 px-3 hover:bg-[#f7f7f5] rounded-md flex gap-4 items-center font-bold text-sm' to={to}>
+    <Link className='py-2 px-3 hover:bg-[#cececb] rounded-md flex gap-4 items-center font-bold text-sm' to={to}>
       {children}
     </Link>
+  )
+}
+export const CustomNavLink = ({ children, to }) => {
+  return (
+    <NavLink className={({ isActive }) => (isActive ? 'bg-[#ECECEC] ' : '') + 'py-2 px-3 hover:bg-[#F3F3F3] rounded-md flex gap-4 items-center font-bold text-sm'} to={to} activeClassName='bg-[#F3F3F3]'>
+      {children}
+    </NavLink>
   )
 }
