4fc7c6afc6f3e51c373d5edd231d2eeb8a9b8acf
Raisn.io <mseresagusil@gmail.com>
2024-01-31 22:51:51 +0100

    deleted search from the component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,12 @@
 import { Button } from './Button'
 import { AddIcon } from './Icons'
 
-export const Header = ({ pageName, buttonText, className, visibleBtn, visibleSearch, setsearchQuery }) => {
+export const Header = ({ pageName, buttonText, className }) => {
   return (
-    <header className={`flex gap-4  p-5 ${className}`}>
+    <header className={`flex gap-4 mb-5 ${className}`}>
       <h1 className='flex-1 font-bold text-xl'>{pageName}</h1>
-      {visibleSearch && (
-        <form action=''>
-          <input onChange={setsearchQuery} className='px-4 py-2 rounded-md' type='text' placeholder='Buscar...' />
-        </form>
-      )}
 
-      {visibleBtn && (
+      {buttonText && (
         <Button>
           <AddIcon />
           {buttonText}
