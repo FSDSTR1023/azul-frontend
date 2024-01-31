a100c1fac07b4ea95dab1a7c9aed8c95a1c4af75
Raisn.io <mseresagusil@gmail.com>
2024-01-31 22:52:12 +0100

    removedd header

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,6 @@ export const Layout = ({ children, pageName, buttonText, visibleBtn, visibleSear
   return (
     <div id='layout' className='w-full h-screen'>
       <AsideNav className='[grid-area:aside]' />
-
       <main className='[grid-area:main] p-5'>{children}</main>
     </div>
   )
