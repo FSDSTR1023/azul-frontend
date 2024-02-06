edb1e47c8f7bc3d27331bdf97774aace595aee38
Raisn.io <mseresagusil@gmail.com>
2024-02-06 23:20:28 +0100

    added

--- src/api/maquinas.js
+++ src/api/maquinas.js
@@ -1,4 +1,5 @@
 import axios from './axios'
 
 export const getAllMachines = () => axios.get('/machine')
-export const getMachine = id => axios.get(`/machine/${id}`)
+export const getMachine = id => axios.get(`/machine?id=${id}`)
+export const createMachine = data => axios.post('/machine/', data)
