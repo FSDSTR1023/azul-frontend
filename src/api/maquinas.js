fdde0fbd6d0b3af89fb6a4238321613f02633773
Raisn.io <mseresagusil@gmail.com>
2024-02-13 21:51:49 +0100

    added the update machine api route

--- src/api/maquinas.js
+++ src/api/maquinas.js
@@ -1,5 +1,6 @@
 import axios from './axios'
 
 export const getAllMachines = () => axios.get('/machine')
-export const getMachine = id => axios.get(`/machine?id=${id}`)
+export const getMachine = id => axios.get(`/machine/${id}`)
 export const createMachine = data => axios.post('/machine/', data)
+export const updateMachine = (id, data) => axios.put(`/machine/${id}`, data)
