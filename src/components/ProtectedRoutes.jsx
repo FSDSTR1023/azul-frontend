002777b73e9c36cf2d6ac3bfe08cf9146b199bf8
Raisn.io <mseresagusil@gmail.com>
2024-02-16 20:19:54 +0100

    Added Loading on login and on checking protected routes

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -1,10 +1,11 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
+import { Loader } from './Loader'
 
 export const ProtectedRoutes = () => {
   const { isAuthenticated, isLoading } = useAuth()
   console.log(isAuthenticated, isLoading)
-  if (isLoading) return <h1>Cargando...</h1>
+  if (isLoading) return <Loader />
   if (!isLoading && !isAuthenticated) return <Navigate to='/login' replace />
   return <Outlet />
 }
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,8 +3,11 @@ import { Button } from '../components/Button/Button'
 import { Input } from '../components/Forms/Input'
 import { useAuth } from '../context/AuthContext'
 import { Navigate } from 'react-router'
+import { useState } from 'react'
+import { Loader } from '../components/Loader'
 
 export const LoginPage = () => {
+  const [isLoading, setIsLoading] = useState(false)
   const {
     handleSubmit,
     register,
@@ -16,32 +19,44 @@ export const LoginPage = () => {
     return <Navigate to='/alquileres' replace />
   }
   const onSubmit = handleSubmit(async (data) => {
-    console.log(data)
-    signin(data)
+    setIsLoading(true)
+    try {
+      const res = await signin(data)
+      if (res) setIsLoading(false)
+    } catch (error) {
+      setIsLoading(false)
+    }
   })
   return (
-    <div className='w-full bg-white p-5 max-w-xl rounded'>
-      <p className='text-left text-slate-400'>Entra tus datos para realizar el login</p>
-      <form onSubmit={onSubmit} className='flex flex-col items-start mt-3 gap-3'>
-        <Input
-          label='Email'
-          placeholder='johndoe@doe.com'
-          type='text'
-          errors={errors}
-          name='email'
-          register={register('email', { required: 'El email es requerido' })}
-        />
-        <Input
-          label='Contraseña'
-          placeholder='Contraseña'
-          type='password'
-          errors={errors}
-          register={register('password', { required: 'La contraseña es requerida' })}
-        />
-        <Button className='mt-2' type='submit'>
-          Iniciar sesión
-        </Button>
-      </form>
-    </div>
+    <>
+      {
+  isLoading && (
+    <Loader />
+  )
+}
+      <div className='w-full bg-white p-5 max-w-xl rounded'>
+        <p className='text-left text-slate-400'>Entra tus datos para realizar el login</p>
+        <form onSubmit={onSubmit} className='flex flex-col items-start mt-3 gap-3'>
+          <Input
+            label='Email'
+            placeholder='johndoe@doe.com'
+            type='text'
+            errors={errors}
+            name='email'
+            register={register('email', { required: 'El email es requerido' })}
+          />
+          <Input
+            label='Contraseña'
+            placeholder='Contraseña'
+            type='password'
+            errors={errors}
+            register={register('password', { required: 'La contraseña es requerida' })}
+          />
+          <Button className='mt-2' type='submit'>
+            Iniciar sesión
+          </Button>
+        </form>
+      </div>
+    </>
   )
 }
