7212594f01e380c3dc3e782337d23aca963f4e41
Raisn.io <mseresagusil@gmail.com>
2024-02-17 12:55:50 +0100

    added api endpoints

--- src/api/usuarios.js
+++ src/api/usuarios.js
@@ -1,5 +1,7 @@
 import axios from './axios'
 
-export const getAllUsers = () => axios.get('/user/users')
-export const getUser = id => axios.get(`/user/${id}`)
 export const createUser = data => axios.post('/user/register/', data)
+export const getUser = id => axios.get(`/user/${id}`)
+export const updateUser = (id, data) => axios.put(`/user/update/${id}`, data)
+export const deleteUser = id => axios.delete(`/user/delete/${id}`)
+export const getAllUsers = () => axios.get('/user/users')
