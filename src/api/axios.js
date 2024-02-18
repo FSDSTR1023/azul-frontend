afbc7d9a2676736ec6b31412bdbd39c7432ba72b
Raisn.io <mseresagusil@gmail.com>
2024-02-18 21:10:30 +0100

    added on every call the bearer

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,19 @@
 import axios from 'axios'
 
+const localStorage = window.localStorage
+const validToken = () => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    return token
+  }
+  return ''
+}
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
   headers: {
-    'Content-type': 'application/json'
+    'Content-type': 'application/json',
+    Authorization: 'Bearer ' + validToken()
   }
 })
 
