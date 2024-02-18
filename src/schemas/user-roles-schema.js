6fa867692483d7236d79b611773a85118fe59b14
Raisn.io <mseresagusil@gmail.com>
2024-02-18 11:16:16 +0100

    correct user schema creation

--- src/schemas/user-roles-schema.js
+++ src/schemas/user-roles-schema.js
@@ -1,1 +1,28 @@
-export const userRoles = ['Cliente', 'Admin', 'Atención al Cliente']
+export const CLIENT = {
+  value: 'User',
+  text: 'Cliente'
+}
+export const ADMIN = {
+  value: 'Admin',
+  text: 'Admin'
+}
+export const AT = {
+  value: 'AT',
+  text: 'Atención al Cliente'
+}
+
+export const userRoles = [
+  {
+    value: CLIENT.value,
+    text: CLIENT.text
+  },
+  {
+    value: ADMIN.value,
+    text: ADMIN.text
+  },
+  {
+    value: AT.value,
+    text: AT.text
+  }
+
+]
