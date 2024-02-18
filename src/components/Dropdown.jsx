49dd38ce3068b4e20be81a11d1646178fed7fcd4
Raisn.io <mseresagusil@gmail.com>
2024-02-18 21:07:51 +0100

    check if has enought space and shows up or down

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -21,14 +21,29 @@ export const Dropdown = ({ buttonText = 'Open', children, dynamicText, variant }
       document.removeEventListener('mousedown', handleClickOutside)
     }
   }, [])
+
+  useEffect(() => {
+    if (isOpen && dropdownRef.current) {
+      const tableRect = dropdownRef.current.closest('table').getBoundingClientRect()
+      const dropdownRect = dropdownRef.current.getBoundingClientRect()
+      const bottomSpace = tableRect.bottom - dropdownRect.bottom
+      const dropdownHeight = dropdownRect.height
+      if (bottomSpace <= dropdownHeight) {
+        dropdownRef.current.style.top = `-${dropdownHeight}px`
+      } else {
+        dropdownRef.current.style.top = '100%'
+      }
+    }
+  }, [isOpen])
+
   const toggleDropdown = () => {
     setIsOpen(!isOpen)
   }
   return (
-    <div className='relative' ref={dropdownRef}>
+    <div className='relative'>
       <Button variant={variant} className='relative capitalize' onClick={toggleDropdown}>{dynamicText || buttonText} <ChevronDown /></Button>
       {isOpen && (
-        <ul onClick={() => setIsOpen(false)} className='flex flex-col bg-slate-50 absolute top-10 z-10 rounded border shadow-md'>
+        <ul ref={dropdownRef} onClick={() => setIsOpen(false)} className='flex flex-col bg-slate-50 absolute w-max top-10 right-0 z-10 rounded border shadow-md'>
           {children}
         </ul>
       )}
