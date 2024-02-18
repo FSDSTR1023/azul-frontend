83cd757d6ed64840d7b52cff4bee0ea7c25838e2
Raisn.io <mseresagusil@gmail.com>
2024-02-18 21:10:17 +0100

    added routes to get profile and update data

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,4 +3,5 @@ import axios from './axios'
 export const registerReq = user => axios.post('/user', user)
 export const loginReq = user => axios.post('/user/login', user)
 export const verifyTokenReq = token => axios.post('/user/verify', { token })
-export const getProfileReq = () => axios.get('/user/profile')
+export const getProfileReq = () => axios.get('/user/profile/')
+export const updateProfileReq = data => axios.put('/user/profile/update', data)
