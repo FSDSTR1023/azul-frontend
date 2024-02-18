f5c448e6907047b091bc29fe6bbc25a752ae09c5
Raisn.io <mseresagusil@gmail.com>
2024-02-18 23:22:23 +0100

    added routes to the api

--- src/api/incidencias.js
+++ src/api/incidencias.js
@@ -2,3 +2,5 @@ import axios from './axios'
 
 export const getAllIncidents = () => axios.get('/incident')
 export const getIncident = id => axios.get(`/incident/${id}`)
+export const createIncident = data => axios.post('/incident', data)
+export const closeIncident = id => axios.put(`/incident/close/${id}`)
