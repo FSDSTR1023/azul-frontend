8f41593df05d93d7b5421df79a117c12a6596759
Raisn.io <mseresagusil@gmail.com>
2024-02-18 23:22:09 +0100

    dropdown positioned dinamically with the absolute position of the buttonm

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -6,44 +6,30 @@ export const Dropdown = ({ buttonText = 'Open', children, dynamicText, variant }
   const [isOpen, setIsOpen] = useState(false)
   const dropdownRef = useRef(null)
   useEffect(() => {
+    if (isOpen && dropdownRef.current) {
+      const buttonRect = dropdownRef.current.previousElementSibling.getBoundingClientRect()
+      dropdownRef.current.style.top = `${buttonRect.top + buttonRect.height}px`
+      dropdownRef.current.style.left = `${buttonRect.left}px`
+    }
+  }, [isOpen])
+  useEffect(() => {
     // Function to close dropdown when clicked outside
     const handleClickOutside = (event) => {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
         setIsOpen(false)
       }
     }
-
-    // Add event listener when component mounts
     document.addEventListener('mousedown', handleClickOutside)
-
-    // Remove event listener when component unmounts
     return () => {
       document.removeEventListener('mousedown', handleClickOutside)
     }
   }, [])
 
-  useEffect(() => {
-    if (isOpen && dropdownRef.current) {
-      const tableRect = dropdownRef.current.closest('table').getBoundingClientRect()
-      const dropdownRect = dropdownRef.current.getBoundingClientRect()
-      const bottomSpace = tableRect.bottom - dropdownRect.bottom
-      const dropdownHeight = dropdownRect.height
-      if (bottomSpace <= dropdownHeight) {
-        dropdownRef.current.style.top = `-${dropdownHeight}px`
-      } else {
-        dropdownRef.current.style.top = '100%'
-      }
-    }
-  }, [isOpen])
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen)
-  }
   return (
     <div className='relative'>
-      <Button variant={variant} className='relative capitalize' onClick={toggleDropdown}>{dynamicText || buttonText} <ChevronDown /></Button>
+      <Button variant={variant} className='relative capitalize' onClick={() => setIsOpen(!isOpen)}>{dynamicText || buttonText} <ChevronDown /></Button>
       {isOpen && (
-        <ul ref={dropdownRef} onClick={() => setIsOpen(false)} className='flex flex-col bg-slate-50 absolute w-max top-10 right-0 z-10 rounded border shadow-md'>
+        <ul ref={dropdownRef} className='flex flex-col bg-slate-50 fixed w-max top-10 z-10 rounded border shadow-md'>
           {children}
         </ul>
       )}
