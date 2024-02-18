6025e60d4389dd1b8f011775845016540dac7672
Raisn.io <mseresagusil@gmail.com>
2024-02-18 21:12:30 +0100

    added dependency to edit profile

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,13 @@
-// import { Header } from './Header'
 import { AsideNav } from './AsideNav'
 import { Loader } from './Loader'
 
-export const Layout = ({ children, isLoading }) => {
+export const Layout = ({ children, isLoading, handleEditUser }) => {
   return (
     <div id='layout' className='w-full h-screen'>
       {
         isLoading && <Loader />
       }
-      <AsideNav className='[grid-area:aside]' />
+      <AsideNav className='[grid-area:aside]' handleEditUser={handleEditUser} />
       <main className='[grid-area:main] p-5 overflow-auto'>{children}</main>
     </div>
   )
