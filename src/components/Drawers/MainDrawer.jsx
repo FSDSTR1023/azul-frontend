092a143848ff9754dfe29df6b369e83ee3dcb3c6
Raisn.io <mseresagusil@gmail.com>
2024-02-25 21:49:54 +0100

    added styling to the new buttons section

--- src/components/Drawers/MainDrawer.jsx
+++ src/components/Drawers/MainDrawer.jsx
@@ -15,6 +15,7 @@ export const MainDrawer = ({ children, isOpen, toggleDrawer, title, resetDrawerI
       }, 500)
     }
   }, [isOpen])
+  console.log(stateButton)
   return (
     <div id='drawer' className={`${isOpen ? 'open' : ''} fixed top-0 right-0 w-full h-screen pointer-events-none`}>
       <div className='w-full md:w-[480px] p-7 h-screen bg-white absolute right-0 shadow-lg pointer-events-auto z-10 flex flex-col'>
@@ -26,11 +27,11 @@ export const MainDrawer = ({ children, isOpen, toggleDrawer, title, resetDrawerI
           {mounted && children}
         </div>
         {showButton &&
-          <div className='flex pt-4 gap-4'>
+          <div className='flex flex-col pt-4 gap-4'>
             <Button className='w-full text-base font-medium' onClick={submitForm}>{title}</Button>
             {mode === 'edit' && (
-              <div>
-                <Button variant='outline' className='w-full text-base font-medium' onClick={handleState}>{stateButton ? 'Habilitar' : 'Deshabilitar'}</Button>
+              <div className='flex gap-4'>
+                <Button variant='outline' className='w-full text-base font-medium' onClick={handleState}>{stateButton ? 'Deshabilitar' : 'Habilitar'}</Button>
                 <Button variant='danger' className='w-full text-base font-medium' onClick={handleDelete}>Eliminar</Button>
               </div>
             )}
