7c71455c5329ca11a27ae1c93c92063d99689200
Raisn.io <mseresagusil@gmail.com>
2024-02-27 19:43:11 +0100

    Add user registration token verification

--- src/api/usuarios.js
+++ src/api/usuarios.js
@@ -3,3 +3,4 @@ import axios from './axios'
 export const getAllUsers = () => axios.get('/user/users')
 export const getUser = id => axios.get(`/user/${id}`)
 export const createUser = data => axios.post('/user/register/', data)
+export const verifyTokenRegisterReq = token => axios.post('/user/verify-registration', { token })
