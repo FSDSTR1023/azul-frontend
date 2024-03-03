cb1cc5a1cbd7bb588d14c47feb32afabecf4913b
Raisn.io <mseresagusil@gmail.com>
2024-03-03 11:46:31 +0100

    Update socket URL in Notifications.jsx

--- src/context/Notifications.jsx
+++ src/context/Notifications.jsx
@@ -4,7 +4,7 @@ import { toast } from 'sonner'
 
 export const Notifications = () => {
   useEffect(() => {
-    const socket = io(import.meta.env.VITE_SOCKET_URL)
+    const socket = io(import.meta.env.VITE_API_BASE_URL)
 
     socket.on('userCreated', () => {
       console.log('New user created')
