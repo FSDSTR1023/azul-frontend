0eee64bb4262698829a7e9d1c56a594ee037d92b
Raisn.io <mseresagusil@gmail.com>
2024-03-03 12:57:58 +0100

    Add extraClassName prop to Pill component

--- src/components/Pill.jsx
+++ src/components/Pill.jsx
@@ -1,5 +1,5 @@
-export const Pill = ({ children, color }) => {
+export const Pill = ({ children, color, extraClassName }) => {
   return (
-    <span style={{ backgroundColor: color }} className='py-2 px-3 rounded-full font-semibold text-black whitespace-nowrap w-max'>{children}</span>
+    <span style={{ backgroundColor: color }} className={`py-2 px-3 rounded-full font-semibold text-black whitespace-nowrap w-max ${extraClassName}`}>{children}</span>
   )
 }
