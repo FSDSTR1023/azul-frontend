ab3f01f7ad3c78812350267eb9f1fc59da7a109e
Raisn.io <mseresagusil@gmail.com>
2024-03-04 19:20:49 +0100

    Refactor axios.js to use request interceptor for token authorization

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,20 +1,27 @@
 import axios from 'axios'
 
 const localStorage = window.localStorage
-const validToken = () => {
-  const token = localStorage.getItem('token')
-  if (token) {
-    return token
-  }
-  return ''
-}
+
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
   headers: {
-    'Content-type': 'application/json',
-    Authorization: 'Bearer ' + validToken()
+    'Content-type': 'application/json'
   }
 })
+instance.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error)
+  }
+)
 
 export default instance
